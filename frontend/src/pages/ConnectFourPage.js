bee567e8bb3586a4bcc091ab635fef4adfe2abbf
Tristan Steffen <tristansteffen@gmx.de>
2024-05-23 16:48:06 +0200

    connect 4 placeholder

---
+++ frontend/src/pages/ConnectFourPage.js
@@ -0,0 +1,13 @@
+// src/pages/ConnectFourPage.js
+import React from 'react';
+
+const ConnectFourPage = () => {
+    return (
+        <div>
+            <h1>Vier Gewinnt</h1>
+            <p>Das Spiel wird bald verfügbar sein.</p>
+        </div>
+    );
+};
+
+export default ConnectFourPage;
