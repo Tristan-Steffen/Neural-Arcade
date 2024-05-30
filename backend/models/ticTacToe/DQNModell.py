6fed1cd793855a70f5faaa8bff33facc27e613e7
Tristan Steffen <tristansteffen@gmx.de>
2024-05-30 18:11:39 +0200

    finally made the DQN work

--- backend/models/ticTacToe/DQNModell.py
+++ backend/models/ticTacToe/DQNModell.py
@@ -1,7 +1,7 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow. import Sequential 
-from keras.layers import Dense, Flatten 
+import keras
+from keras import Sequential
+from keras.src.layers import Flatten, Dense
 from collections import deque
 import random
 
@@ -10,7 +10,7 @@ class DQNAgent:
         self.gamma = 0.95
         self.epsilon = 1.0
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_decay = 0.99
         self.learning_rate = 0.001
         self.model = self.build_model()
         self.target_model = self.build_model()
@@ -19,33 +19,44 @@ class DQNAgent:
 
     def build_model(self):
         model = Sequential([
-            Flatten(input_shape=(3, 3)),
-            Dense(128, activation='relu'),
-            Dense(64, activation='relu'),
+            Flatten(input_shape=(10,)),  # Flattening input shape (9,)
+            Dense(36, activation='relu'),
+            Dense(36, activation='relu'),
             Dense(9, activation='linear')
         ])
-        model.compile(optimizer=tf.keras.optimizers.Adam(self.learning_rate), 
+        model.compile(optimizer=keras.optimizers.Adam(self.learning_rate), 
                       loss='mse')
         return model
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
-            return random.choice(np.where(state.flatten() == 0)[0])
-        act_values = self.model.predict(state.reshape(1, 3, 3))
+            return random.choice(np.where(np.array(state) == 0)[0])
+        act_values = self.model.predict(np.array(state).reshape(1, 10), verbose=0)
         return np.argmax(act_values[0])
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
     def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                target += self.gamma * np.amax(self.target_model.predict(next_state.reshape(1, 3, 3))[0])
-            target_f = self.model.predict(state.reshape(1, 3, 3))
-            target_f[0][action] = target
-            self.model.fit(state.reshape(1, 3, 3), target_f, epochs=1, verbose=0)
+        states = np.array([transition[0] for transition in minibatch])
+        actions = np.array([transition[1] for transition in minibatch])
+        rewards = np.array([transition[2] for transition in minibatch])
+        next_states = np.array([transition[3] for transition in minibatch])
+        dones = np.array([transition[4] for transition in minibatch])
+
+        next_q_values = self.target_model.predict(next_states, verbose=0)
+        targets = rewards + self.gamma * np.amax(next_q_values, axis=1) * (1 - dones)
+        target_f = self.model.predict(states, verbose=0)
+
+        for i, action in enumerate(actions):
+            target_f[i][action] = targets[i]
+
+        self.model.fit(states, target_f, epochs=1, verbose=1)
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
---
+++ backend/models/ticTacToe/DQNtrainer.py
@@ -0,0 +1,39 @@
+from games import TicTacToe
+from models.ticTacToe.DQNModell import DQNAgent
+import keras
+import os
+
+EPISODES = 300
+game = TicTacToe.TicTacToe()
+agent = DQNAgent()
+
+for e in range(EPISODES):
+    game.reset()
+    state = game.board.tolist() + [game.current_player]
+    done = False
+    while not done:
+        action = agent.act(state)
+        next_state = game.board.tolist() + [game.current_player]
+        winner = game.check_winner()
+        reward = 0
+        if game.move(action):
+            if winner is not None:
+                if winner == 0:
+                    reward = 0.5  # Draw
+                else:
+                    reward = 1 if winner == 1 else -1
+                done = True
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+        else:
+            reward = -10
+            done = True
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+    agent.replay(100)
+    agent.update_target_model()
+    if e % 25 == 0:
+        print(f"Episode {e}/{EPISODES}, Epsilon: {agent.epsilon}")
+        
+# Modell speichern
+keras.saving.save_model(agent.model, os.path.join("DQNtrainedModels", "DQNModell.h5"))
